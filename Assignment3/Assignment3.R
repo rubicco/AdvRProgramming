7a37b418bab99695b0c7e028722ad3eacccbaaa0
stasinak <andreasstasi@hotmail.gr>
2018-09-12 19:49:00 +0200

    Dijkstra function

--- Assignment3/Assignment3.R
+++ Assignment3/Assignment3.R
@@ -61,7 +61,7 @@ dijkstra <- function(graph, init_node)
     stop("'graph' argument is NOT a data.frame!")
   if(NCOL(graph)!=3) 
     stop("'graph' argument has more (or less) than 3 columns!")
-  if(!is.numeric(init_code) | length(init_node)!=1) 
+  if(!is.numeric(init_node) | length(init_node)!=1) 
     stop("'init_node' argument is not a scalar!")
   
   colnames(graph) <- c("v1", "v2", "w")
@@ -73,13 +73,32 @@ dijkstra <- function(graph, init_node)
   #d_matrix[,1] <- values
   d_matrix[init_node,1] <- 0
   unvisited <- values
-  curr_node = init_node
+  curr_node <- init_node
   while(length(unvisited)!=0) 
   {
-    indexes = c(which(graph$v2==curr_node | graph$v1==curr_node))
+    indexes <- which(graph$v2==curr_node | graph$v1==curr_node)
     #temp <- graph[indexes,]
-    c(graph$v1[which(graph$v2==curr_node)], graph$v2[which(graph$v1==curr_node)])
-    curr_node
+    neighbours <- rep(NA, length(indexes))
+    w_i <- graph$w[indexes] + d_matrix[curr_node,1]
+    for(i in 1:length(indexes))
+    {
+      if(graph$v1[indexes[i]]!=curr_node) neighbours[i] <- graph$v1[indexes[i]]
+      else neighbours[i] <- graph$v2[indexes[i]]
+      if(d_matrix[neighbours[i],1]>w_i[i]) 
+      {
+        d_matrix[neighbours[i],1] <- w_i[i]
+        d_matrix[neighbours[i],2] <- neighbours[i]
+      }
+    }
+    curr_node <- which(d_matrix[, 1] == min(d_matrix[unvisited,1]))
+    unvisited <- unvisited[-which(unvisited==curr_node)]
   }
-  unvisited <- unvisited[-which(unvisited==4)]
+  return(d_matrix)
 }
+
+wiki_graph <-
+  data.frame(v1=c(1,1,1,2,2,2,3,3,3,3,4,4,4,5,5,6,6,6),
+             v2=c(2,3,6,1,3,4,1,2,4,6,2,3,5,4,6,1,3,5),
+             w=c(7,9,14,7,10,15,9,10,11,2,15,11,6,6,9,14,2,9))
+dijkstra(wiki_graph, 1)
+dijkstra(wiki_graph, 3)
